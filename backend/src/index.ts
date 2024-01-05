e90320d314697695fde05ca664b1146ebaae1f3f
Barbaro Javier <barbaro.valmaseda@gmail.com>
2024-01-05 21:52:45 +0400

    feat: add user registration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import "dotenv/config";
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 
+import userRoutes from "./routes/users";
+
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
@@ -13,6 +15,8 @@ app.use(cors());
 app.get("/api/test", async (req: Request, res: Response) => {
   res.json({ message: "Hello from Express" });
 });
+
+app.use("/api/users", userRoutes);
 
 app.listen(8000, () => {
   console.log("Server is running on http://localhost:8000");
